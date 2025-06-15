e351dc2a222fbac856ad02d3ba5011c9dc6274d0
Matt Holmes <iam4423@gmail.com>
2025-06-15 19:07:04 +0100

    feat: finished select logic for flavours component

--- assets/js/htmx-json.js
+++ assets/js/htmx-json.js
@@ -12,15 +12,30 @@ htmx.defineExtension('json-enc', {
         xhr.overrideMimeType('text/json');
         let ob = {}
 
-        console.log(parameters.keys())
         parameters.keys().forEach(k => {
-            console.log([k, parameters.get(k)])
-            if (k.endsWith(".int")) {
-                console.log(k.slice(0, -4), ~~parameters.get(k))
-                ob[k.slice(0, -4)] = ~~parameters.get(k)
+            let key = k
+            let array = false
+            let int = false
+
+            if (key.endsWith("[]")) {
+                array = true
+                key = key.slice(0, -2)
+            }
+            if (key.endsWith(".int")) {
+                int = true
+                key = key.slice(0, -4)
+            }
+
+
+            const values = parameters.getAll(k)
+
+            if (array) {
+                ob[key] = []
+                for (let i in values) {
+                    ob[key].push(int ? ~~values[i] : values[i])
+                }
             } else {
-                console.log("no", k, parameters.get(k))
-                ob[k] = parameters.get(k)
+                ob[key] = int ? ~~values[0] : values[0]
             }
         })
 
