1d17936b2e27cec3daf672f0a9790de83af22df2
Matt Holmes <iam4423@gmail.com>
2025-07-31 19:44:31 +0100

    feat: got recipe filters working
    - not the final version as i don't like the current ui but it works for
      now
    - also fixed style on select boxes site wide

--- internal/coffee/controllers/recipe_view.go
+++ internal/coffee/controllers/recipe_view.go
@@ -39,11 +39,11 @@ func (c Controller) NewRecipe(rw http.ResponseWriter, r *http.Request) {
 }
 
 type viewRecipesFilters struct {
-	Coffees     []string
-	Caffiene    map[int]types.CaffieneLevel
-	DrinkTypes  []types.DrinkType
-	BrewerTypes []types.BrewerType
-	Rating      map[int]string
+	Coffees  []string
+	Caffeine map[int]types.CaffeineLevel
+	Drinks   []types.DrinkType
+	Brewers  []string
+	Rating   map[int]string
 }
 
 type viewRecipesData struct {
@@ -55,18 +55,24 @@ type viewRecipesData struct {
 func (c Controller) ViewRecipes(rw http.ResponseWriter, r *http.Request) {
 	user := r.Context().Value("user").(*auth.User)
 	recipes := c.repo.IndexRecipesForUser(user)
-	pageData := viewRecipesData{
+
+	ui.RenderUser(rw, r, viewRecipesData{
 		PageData: ui.NewPageData("Recipes", "recipes", user),
 		Recipes:  recipes,
 		Filters: viewRecipesFilters{
-			Coffees: extractRecipeCoffees(recipes),
-			Caffiene: map[int]types.CaffieneLevel{
+			Coffees: extractRecipe(recipes, func(r coffee.Recipe) *string { return &r.Coffee.Name }),
+			Caffeine: map[int]types.CaffeineLevel{
 				1: types.CafLevelFull,
 				2: types.CafLevelHalf,
 				3: types.CafLevelDecaf,
 			},
-			DrinkTypes:  types.Drinks,
-			BrewerTypes: types.Brewers,
+			Drinks: types.Drinks,
+			Brewers: extractRecipe(recipes, func(r coffee.Recipe) *string {
+				if r.Brewer == nil {
+					return nil
+				}
+				return &r.Brewer.Name
+			}),
 			Rating: map[int]string{
 				1: "1 Star",
 				2: "2 Stars",
@@ -75,24 +81,26 @@ func (c Controller) ViewRecipes(rw http.ResponseWriter, r *http.Request) {
 				5: "5 Stars",
 			},
 		},
-	}
-
-	ui.RenderUser(rw, r, pageData)
+	})
 }
 
-func extractRecipeCoffees(recipes []coffee.Recipe) []string {
-	var seen map[string]struct{}
-	var coffees []string
+func extractRecipe(recipes []coffee.Recipe, cb func(coffee.Recipe) *string) []string {
+	var values []string
+	seen := make(map[string]struct{})
 
 	for _, recipe := range recipes {
-		if _, found := seen[recipe.Coffee.Name]; found {
+		value := cb(recipe)
+		if value == nil {
+			continue
+		}
+		if _, found := seen[*value]; found {
 			continue
 		}
 
-		seen[recipe.Coffee.Name] = struct{}{}
-		coffees = append(coffees, recipe.Coffee.Name)
+		seen[*value] = struct{}{}
+		values = append(values, *value)
 	}
 
-	slices.Sort(coffees)
-	return coffees
+	slices.Sort(values)
+	return values
 }
